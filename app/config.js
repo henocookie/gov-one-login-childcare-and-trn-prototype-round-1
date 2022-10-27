ae76b5ac090f0248b9aef2fcdaf51a50d1b63fb0
Henry Cookson <52792328+henocookie@users.noreply.github.com>
2022-10-27 10:58:35 +0100

    update index page heading to signify this prototype was from round 1 of user research

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'GOV.UK prototype',
+  serviceName: 'Prototype: GOV.UK One Login service header and dashboard',
 
   // Default port that prototype runs on
   port: '3000',
